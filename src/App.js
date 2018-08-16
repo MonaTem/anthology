59696ab2cbfa1ddd51ea6d3aa0435907b2e0fcd7
Mona Temchin <monatemchin@gmail.com>
2018-08-16 14:18:25 -0400

    Added routes to different page components

--- src/App.js
+++ src/App.js
@@ -14,6 +14,14 @@ class App extends Component {
           <div>
             <Route exact path="/" component={Home}></Route>
             <Route exact path="/About" component={About}></Route>
+            <Route exact path="/Description" component={Description}></Route>
+            <Route exact path="/About" component={About}></Route>
+            <Route exact path="/TableOfContents" component={TableOfContents}></Route>
+            <Route exact path="/Authors" component={Authors}></Route>
+            <Route exact path="/Author" component={Author}></Route>
+            <Route exact path="/Imprint" component={Imprint}></Route>
+            <Route exact path="/Contact" component={Contact}></Route>
+            <Route exact path="/Buy" component={Buy}></Route>
            </div>
         </Router>
       </div>
