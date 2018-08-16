9c04a02ea51a38a09f2d28fbe37cf0de0fdfda8a
Mona Temchin <monatemchin@gmail.com>
2018-08-16 15:29:28 -0400

    Added Routes, Switch to React-Router.  Added empty component.js files to components folder. Renamed App.js and App.css to Anthology.js and Anthology.css All styles moved to styles folder

---
+++ src/Anthology.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './styles/Anthology.css';
+import { BrowserRouter as Router, Route, Swtich } from "react-router-dom";
+
+class Anthology extends Component {
+
+
+  render() {
+    return (
+      <div className="App">
+      <br/>
+      <br/>
+        <Router>
+          <div>
+          <Switch>
+            <Route exact path="/" component={Home}></Route>
+            <Route path="/description" component={Description}></Route>
+            <Route path="/tableocontents" component={TableOfContents}></Route>
+            <Route path="/authors/:author" component={Author}></Route>
+            <Route exact path="/authors" component={Author}></Route>
+            <Route path="/imprint" component={Imprint}></Route>
+            <Route path="/about" component={About}></Route>
+            <Route path="/contact" component={Contact}></Route>
+            <Route path="/buy" component={Buy}></Route>
+          </Switch>
+           </div>
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default App;
