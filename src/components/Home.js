806e183fb39c328d1862e6a5b29eeadd27da30b3
Mona Temchin <monatemchin@gmail.com>
2018-08-16 16:12:01 -0400

    Added NavTop and NavBottom js components.  Added some styles to Home.CSS (many will be moved elsewhere, including to styles for NavTop and NavBottom)

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles/Anthology.css';
+import './styles/Home.css';
+import { Link } from "react-router-dom";
+
+const Home = (props) => {
+
+    return (
+    <div>
+
+
+             </div>
+    );
+
+}
+
+export default Home;
