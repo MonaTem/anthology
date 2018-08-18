42241ae28fc1d0122e89346363b0db178018f3e3
Mona Temchin <monatemchin@gmail.com>
2018-08-18 00:32:28 -0400

    Added Bootstrap elements to make top navbar responsive

--- src/components/LinksTop.js
+++ src/components/LinksTop.js
@@ -4,27 +4,35 @@ import '../styles/LinksTop.css';
 import {Link} from 'react-router-dom';
 
 const LinksTop = (props) => {
-    
-      return (<div>
-        <nav>
-          <p id="home">
-            <Link to={"/"} style={props.navstylehome}>HOME</Link>
-          </p>
-          <p id="description">
-            <Link to={"/description"} style={props.navstyledesc}>DESCRIPTION</Link>
-          </p>
-          <p id="table-of-contents">
-            <Link to={"/tableofcontents"} style={props.navstyletoc}>TABLE OF CONTENTS</Link>
-          </p>
-          <p id="authors">
-            <Link to={"/authors"} style={props.navstyleauthors}>AUTHORS</Link>
-          </p>
-          <p id="buy">
-            <Link to={"/buy"} style={props.navstylebuy}>BUY</Link>
-          </p>
-        </nav>
-      </div>);
 
+  var bgcolor = {
+    backgroundColor: 'rgb(115, 179, 222)'
+  }
+
+  return (<div>
+    <nav className="navbar-expand-lg navbar-light navbar-toggleable-lg" style={bgcolor}>
+      <button class="navbar navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+       <span class="navbar navbar-toggler-icon"></span>
+      </button>
+      <div class="navbar navbar-nav">
+      <p id="home">
+        <Link to={"/"} style={props.navstylehome}>HOME</Link>
+      </p>
+      <p id="description">
+        <Link to={"/description"} style={props.navstyledesc}>DESCRIPTION</Link>
+      </p>
+      <p id="table-of-contents">
+        <Link to={"/tableofcontents"} style={props.navstyletoc}>TABLE OF CONTENTS</Link>
+      </p>
+      <p id="authors">
+        <Link to={"/authors"} style={props.navstyleauthors}>AUTHORS</Link>
+      </p>
+      <p id="buy">
+        <Link to={"/buy"} style={props.navstylebuy}>BUY</Link>
+      </p>
+    </div>
+    </nav>
+  </div>);
 
 }
 
