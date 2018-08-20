2f9637d8e736066573b75669897b11c513b24e5a
Mona Temchin <monatemchin@gmail.com>
2018-08-19 20:24:04 -0400

    Added flexbox to quotes on right side of home page to improve responsiveness

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,7 @@ const Home = (props) => {
     <div>
       <img id="cover-001" className="img-fluid" src="/images/FrontCover.jpeg" alt="Anthology Front Cover"/>
       <img id="cover-002" className="img-fluid" src="/images/BackCover.jpeg" alt="Anthology Back Cover"/>
-      <section id="most-powerful-section">
+      <section id="most-powerful-section" className="d-flex flex-column">
         <p id="most-powerful">The most powerful person in the world
           <br></br>is the storyteller. The storyteller sets the
           <br></br>vision, values, and agenda for an entire
@@ -23,7 +23,7 @@ const Home = (props) => {
         <p id="steve-jobs">Steve Jobs</p>
       </section>
       <section>
-        <div id="group-4">
+        <div id="group-4" className="d-flex flex-column">
           <p id="if-you-want-to-learn">If you want to learn about a CULTURE,
             <br></br>listen to the stories.If you want to change
             <br></br>the culture, change the stories.
@@ -33,7 +33,7 @@ const Home = (props) => {
           </p>
         </div>
       </section>
-      <section id="group-3-section">
+      <section id="group-3-section" className="d-flex flex-column">
         <p id="stories-constitute-t">Stories constitute the single most powerful
           <br></br>weapon in a LEADER’s arsenal.
         </p>
@@ -41,7 +41,7 @@ const Home = (props) => {
           <br></br>
         </p>
       </section>
-      <section id="its-like-everyone-t-section">
+      <section id="its-like-everyone-t-section" className="d-flex flex-column">
         <br></br>
         <p id="its-like-everyone-t">It’s like everyone tells a story about
           <br></br>themselves inside their own head…
