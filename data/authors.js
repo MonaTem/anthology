30373b1480ac9d40d016db15ab6c93a994f88872
Mona Temchin <monatemchin@gmail.com>
2018-08-20 22:06:23 -0400

    Added author json data for 2 author profiles

---
+++ data/authors.js
@@ -0,0 +1,24 @@
+const authors = [
+  {
+    'name': 'Gene Bellinger',
+    'bio': 'Gene Bellinger, etc. (placeholder for bio)',
+    'photo_url': 'https://lh3.googleusercontent.com/e45xzKITIU3PnH8szs7DucfagOpQ2DqtAoYgNTAHzTaKNe7zbuEE-rdaCaE9mtvPiU9E13MW1xhEFbRSkAy1ie91SV5xWUNyqO4pKLf9ybB-EIwazwjXE0lVTTgxW5gU_h3_xYBNDNCk26aRNOXoZjC7pAS5oxz0aajEHA2i1LsCNgtSkAIJMCPQqy4cEFuRRQaVEKavHSwo1fQOSJCVBcXlyoWSBvgj7kM-OliPMytrDmkSOpGP1IKa2UnC5AAFrsWN7eZ089GA43iNO8WIKnJehSafRur11yny_PVi62iXqbel8Dx4JcMolFJD_46e50ydjLWmgJuC3_CA38dLnVl22OA1HoaUsJVukoi1Nv2aG1SPB9-WbjHdJSXQr90VFa3Sajvnt4zWuNWf0EefagacVGHtVTPHzCHCQ3lPdfN39IRPduJNmjdbQJ7oUVNrTz9FO2p50q-JJ532a_ayjz5ajBA0TJtTwCGzuvpgE-1WPlq8STu2jNejUv-Kn9qgQ5_3HrOxPou_AnbO92OCDN7lzQoRaKyW3-3bkdk7NlhYecrFeN0SPrm9M_aepTc44l-IdE0Mi_jTT-SaqevvVLm_2b5JmZ6jFFsfOF8=s236-no',
+    'video_link': 'https://youtu.be/3Ymt_TbNNwE',
+    'websites': [{'website': 'https://www.linkedin.com/in/systemswiki/'}, {'website': 'https://kumu.io/stw/perspectives'}],
+    'contact': 'systemswiki@gmail.com',
+    'publications': null
+  },
+  {
+    'name': 'Marc Jadoul',
+    'bio': 'Marc Jadoul, etc. (placeholder for bio)',
+    'photo_url': 'https://lh3.googleusercontent.com/gPTN1r5xay4KBZLGv4wLHYICoQw6dyvdDsPljs2HpkOD2fYBpYQOhHBSeYZ4IUzBZSYKORjM4kXVIiwbpmb62nHRkVtG9GOpyEAdVNfA6IKGDVZ4w-E-rD-2p_LS-4g-4ZqOG4YJr2jCMSOF_oUBWckmYfF6S7dB-15ezlaNTKYB68gJ_EZGmZQgLQ5tOGasDeHHmiKI2t9lwqiwhEP4YkTsLh8LA4Yjyv9mAGFg3LpwiDZx8uJSYf--IdamUZuOYpZY4ThYMAIbHMgAX26DGce_lLlcbqFkiZctdPRgOktz7GSOhKo_u1MGS2LJLLa6pqxVIso5DkRVtEFdm9Duauof5t6VZ28HGCdFNwQZz53rrSvUvNufnduW42dLQ0h1DN4-mreO_db7nQ52AR_Yo69lQ5w7uqXx8uKONtv7XbOIxHxR2RohrORI6sKeGKbGVnhVi1f9FTMH8qxCkcWfhSDvcWexdf-kvVug4PxML1Nb5PLtI9oONBtRg2OuMnhYv6JYpczNZSI3wA-3Ri8hj-6_ZM4aY2pJzKLoxOXsZQcCd5LbfjL6-YQ3ExXQz0oB7bZ4QSD94LAcPEeDr_flInmoHW9LUMrLyEXZuc8=w163-h165-no',
+    'video_link': 'https://www.youtube.com/watch?v=MwOaesC01E8',
+    'websites': [{'website': 'https://www.linkedin.com/in/mjadoul/'}, {'website': 'https://www.speakersbase.com/public/mjadoul'},{'website': 'https://b2bstorytelling.wordpress.com/'},{'website':'https://www.business2community.com/author/marc-jadoul'},{'website':'https://www.slideshare.net/mjadoul'}],
+    'contact': 'https://b2bstorytelling.wordpress.com/get-in-touch-with-me/',
+    'publications': [{'publication': 'https://b2bstorytelling.wordpress.com/download/'}, {'publication': 'https://b2bstorytelling.wordpress.com/download/'}]
+  }
+
+];
+
+
+export default authors;
