077fec7932c497d0dbab8eb9baac7304eaeb0d69
Mona Temchin <monatemchin@gmail.com>
2018-08-22 19:58:17 -0400

    Added more author profile JSON data

--- data/authors.js
+++ data/authors.js
@@ -1,6 +1,6 @@
 const authors = [
   {
-    'name': 'Gene Bellinger',
+    'name': 'GENE BELLINGER',
     'bio': 'Gene Bellinger, etc. (placeholder for bio)',
     'photo_url': 'https://lh3.googleusercontent.com/e45xzKITIU3PnH8szs7DucfagOpQ2DqtAoYgNTAHzTaKNe7zbuEE-rdaCaE9mtvPiU9E13MW1xhEFbRSkAy1ie91SV5xWUNyqO4pKLf9ybB-EIwazwjXE0lVTTgxW5gU_h3_xYBNDNCk26aRNOXoZjC7pAS5oxz0aajEHA2i1LsCNgtSkAIJMCPQqy4cEFuRRQaVEKavHSwo1fQOSJCVBcXlyoWSBvgj7kM-OliPMytrDmkSOpGP1IKa2UnC5AAFrsWN7eZ089GA43iNO8WIKnJehSafRur11yny_PVi62iXqbel8Dx4JcMolFJD_46e50ydjLWmgJuC3_CA38dLnVl22OA1HoaUsJVukoi1Nv2aG1SPB9-WbjHdJSXQr90VFa3Sajvnt4zWuNWf0EefagacVGHtVTPHzCHCQ3lPdfN39IRPduJNmjdbQJ7oUVNrTz9FO2p50q-JJ532a_ayjz5ajBA0TJtTwCGzuvpgE-1WPlq8STu2jNejUv-Kn9qgQ5_3HrOxPou_AnbO92OCDN7lzQoRaKyW3-3bkdk7NlhYecrFeN0SPrm9M_aepTc44l-IdE0Mi_jTT-SaqevvVLm_2b5JmZ6jFFsfOF8=s236-no',
     'video_link': 'https://youtu.be/3Ymt_TbNNwE',
@@ -10,9 +10,9 @@ const authors = [
     'publications': null
   },
   {
-    'name': 'Marc Jadoul',
+    'name': 'MARC JADOUL',
     'bio': 'Marc Jadoul (born September 14, 1962) is marketing director at Nokia, spare-time communications consultant, and a passionate B2B storyteller. Over the past decades, Marc has given hundreds of business presentations to tech sector executives worldwide. Author/co-author of ~200 papers, magazine articles and conference talks, and a frequent speaker and panelist at technology industry events, he has earned ‘best speaker’ awards at different occasions. Marc lives in Belgium and speaks Dutch, English and French.',
-    'photo_url': 'https://lh3.googleusercontent.com/gPTN1r5xay4KBZLGv4wLHYICoQw6dyvdDsPljs2HpkOD2fYBpYQOhHBSeYZ4IUzBZSYKORjM4kXVIiwbpmb62nHRkVtG9GOpyEAdVNfA6IKGDVZ4w-E-rD-2p_LS-4g-4ZqOG4YJr2jCMSOF_oUBWckmYfF6S7dB-15ezlaNTKYB68gJ_EZGmZQgLQ5tOGasDeHHmiKI2t9lwqiwhEP4YkTsLh8LA4Yjyv9mAGFg3LpwiDZx8uJSYf--IdamUZuOYpZY4ThYMAIbHMgAX26DGce_lLlcbqFkiZctdPRgOktz7GSOhKo_u1MGS2LJLLa6pqxVIso5DkRVtEFdm9Duauof5t6VZ28HGCdFNwQZz53rrSvUvNufnduW42dLQ0h1DN4-mreO_db7nQ52AR_Yo69lQ5w7uqXx8uKONtv7XbOIxHxR2RohrORI6sKeGKbGVnhVi1f9FTMH8qxCkcWfhSDvcWexdf-kvVug4PxML1Nb5PLtI9oONBtRg2OuMnhYv6JYpczNZSI3wA-3Ri8hj-6_ZM4aY2pJzKLoxOXsZQcCd5LbfjL6-YQ3ExXQz0oB7bZ4QSD94LAcPEeDr_flInmoHW9LUMrLyEXZuc8=w163-h165-no',
+    'photo_url': 'https://lh3.googleusercontent.com/eq_44Mj5chGvVM-ZKWp6ZJNewcqAJlEZqAnc3yXKZP8RiGBZzjz4RuzY0vP1M9J7KHXB00h7m444F350xtWUjvIJL3i1vmA5Zq5x0db1VY2gI84ngCnwJ3s4WtRnI829DVeZpGyxNfrrGkGTKLv9ZddCzyWLKPOwlzmCeIjiEkUS6-qGaskX6LQRriVTLQMk7xiuSaynIxo0mn_1aVFRr9llxlErBoeFYnBX7w32drTa_lP71MZn2In3DXt2EJ_thdlOW-nOs-fLMlfAc4bWu6CIGnQsigyVTjW57QbvVJIseNUc6VL2Bb17iP4xPchvuHoZjn-m8yiD8TWZQCu3GZF3mxBCGws-PLj-P6cM32QNoruWwDbcwmvX6wIuVOSIuALDnAUd92FlxiQ4BkupcYhvhdxPy2gO3CpSRj7u08Z8fCvIqWjlyPkdGv5w_ZTnznWTtkjY36Sw7rR3KHSkKZkdKz_xKzFeDKyhQNCr1jiZ_xXU2E4_QYCeS-3xotdcN-Z_DPDrt_OWkgn6w9YvgTOcJ87AJnW59vevW-o34ftd1ojnJ7Xjt5CLVZrvfvPs5iXaYiiQfJTx2MdUrb5trH7DqiYIP8f7OFj3dpA=s334-no',
     'video_link': 'https://www.youtube.com/watch?v=MwOaesC01E8',
     'video_caption': ['  “7 SINS OF THE SPEAKER"           ',
                       'CAMPUS IN THE CLOUD VIDEO ON        ',
@@ -22,9 +22,9 @@ const authors = [
     'publications': [{'publication': 'https://b2bstorytelling.wordpress.com/download/'}, {'publication': 'https://b2bstorytelling.wordpress.com/download/'}]
   },
   {
-  'name': 'Donald Davis',
+  'name': 'DONALD DAVIS',
   'bio': 'Donald Davis (born 1944) is an American storyteller, author, and minister. Davis had a twenty-year career as a minister before he became a professional storyteller. He has recorded over 25 storytelling albums and written several books. His long career as a teller and his promotion of the cultural importance of storytelling through seminars and master classes has led to Davis being dubbed the "dean of storytelling".',
-  'photo_url': 'https://lh3.googleusercontent.com/HKNBBGLqAg1XUD2DD7e9JB9aHBKwEfWNbwZm6yAt9yAvHt9i4-N04HYp93i9BFbxSOdk6E6A4w=w328-h324-no',
+  'photo_url': 'https://lh3.googleusercontent.com/DClKGmE8a3zr46LmR47q4134x5BQR_yolUyTML6oWcQ9AMefDor8QOvuETCrXQv2iWZTCYga9Fj0WUTVpL-H-4bkKfI5a0lNWc7RLWVlK1sTVWsLTO8e9bEj7-JU1hhhMFpZobpghHQn9usvoChiKwNfUiml36YDALzCNFws2RwCJBcjq7Ipr7-jqHA5Gop0q4qG7ejnjO030v69G4kMsqcD--PaybV9adabssEKJTIcZRGckjFIEaKZAsllx2PXPYUeJYKKa3a-IZBsy4S2jrKmMvCGsCFPr1WezCz4yw-v98_bFiLa8L_rG0xdKZn1dp2vDf2xv6jzLsT7HTPKWA-Jqa1TkindiV-6BvKZ98IOKxMrV6cky_iNdeXH0yYUA0jnnrplQN4g4qwiBiSTNTOg_CWliOCThqnjoL660w5m3MjDiqjdqljmHLPSyekGgeADGYHh_TuVJyGIVzd1s63Qqvs2a8TpRuDUSj2__vo5YZlPz50MlZ20cxZWOnIvkzR4NxjgGWyHc0yxpGCVj65XXl7VaEQPk2w4aqjbFVXEl7x0g7laZh1mXEZKp2ajvY0LNNQZdMK6w1EELi5UEalhCEZz9vVXJyQOreg=w369-h360-no',
   'video_link': 'https://www.youtube.com/watch?v=wgeh4xhSA2Q&feature=youtu.be',
   'video_caption': ['HOW THE STORY TRANSFORMS THE',
                     '           TELLER           ',
@@ -35,9 +35,9 @@ const authors = [
   },
 
   {
-    'name': 'Doug Lipman',
+    'name': 'DOUG LIPMAN',
     'bio': 'Decades ago, I was a very discouraged teacher of emotionally disturbed adolescents. I wanted to be on their side, but all they did was fold their arms and scowl. One day, by accident, I told them a story. They responded! For the duration of the story, we were on the same side! That experience gave me hope, so I renewed my efforts. Months later, we finally became the cooperative classroom I had dreamed of Ever since then, I have pursued the transformative power of storytelling. I pursue it for myself, and work hard to find ever more efficient and satisfying ways to help others master it, too.',
-    'photo_url': 'https://lh3.googleusercontent.com/x4LDGrOPAxFR85cjo5zZc9FK_NvhYkxQqmeMD_HWIy9icaVZd8n8-pVOt4u31M-qIqLy-Um30Q=w327-h325-no',
+    'photo_url': 'https://lh3.googleusercontent.com/BpGjMBD8llm-zy69t5erMY231tses9SaEdXz0Gh7m2xDHiXMc1MawsTKuqo23q29nNeuJk1WeLFCvbqv3Y1sPB7EW7j2WGd73zpGqPq75B_Xx-l-9xok11n--l8biVQN7-JrwqigGqrPHW8TVRp1xnthd0f3yDefp7qxIK8m3Gn2hkhVUokfyMuiPwTGpOER7a-Om--3lq4AQPU4qZ6QxczYPEw3tEkjRkyT-8fJKwS89P1NFzMicXyZLvOkSiGOU1TxBDbTUDOGNOzNC1wmJQTYn4qve4A3NmGLJ3U8qxkNIhjp06oBWs2wOSD8y9iyITGpGOdnLePH4Y14H2uCPGsL28W-F-1e9ai5-aWrRWPk-hdGat7p1_AGde8UDfOM82l2MAbAckN4PH0w7h7t3aHzyHLt9xDXyY5xvZlQBz530EugJDtXNsw0yP5VKsWfxy-xzL4PuwAir6Q4UiMUws8L6im3kGA5k7FWyK57Y0ISy843eLkuNxFSITu1r1dsPfCtowIPd5kxNGD95NiCl1tF8elLT29R7IZCs4M0z3Qhc2B8QBuaoF88ir9bKhMsRfOh75eQSQdM-69LbIGb1U1w3G-jkJUEPgR1kgw=w402-h473-no',
     'video_link': 'https://vimeo.com/73540804',
     'video_caption': ['Why Storytelling Matters:',
                       'Plenary with Doug Lipman'],
@@ -47,16 +47,27 @@ const authors = [
   },
 
   {
-    'name': 'Michael Margolis',
+    'name': 'MICHAEL MARGOLIS',
     'bio': 'Michael Margolis is the CEO and Founder of Get Storied - a storytelling and culture-making company. We serve heads of product, design, and marketing who need to sell a big disruptive future. An advisor to Facebook, Google, and NASA. 2x TEDx speaker. Helping leaders get the sponsorship, alignment, and transformational results they require. All through the power of Storied communications.',
-    'photo_url': 'https://lh3.googleusercontent.com/HgYWB0K7CXk8YlRBSfCaIMKUirHLNeshbMBl5qeFOiS3UoB6T_EDO8QP6CxdqP9wrM21Mak6T8-N6De1t5gaIbR3xQ9m-QM135PzglV4WBhetzQQ4Upr2po_5Fgb-CrxMhNssaoa8IVGepkNt1WMtnZXmkY7sTfXgGbNDsNo-MbdAgZJEEhKNjFVEgFfZJ8l9NqiekO7HVaaFLg3Nj8TbOsa_jgoR_G3XsZf3w-v4AVCIi-Hv0pSrp9f_fOE2XYh82ribplv39_dUfaKFb2EYb2riySeh16W7iweDIokYLBcaWqLo3ZV9JMfT5QWPR67PQ1GTQX6toLqxT-LHAWh9FEwdFVwJXRjDj6BbdMmmq2tX50ig7dx_fw5_S6yLkWPpheUpRRyeAzkJ8F3lDsFWJgNRi_RWcZXSNH3rc7o8OCpcgHVjxrAu6QBMMo0x94gkSs6fMmzVC9rIA9Kr8AAYAhYuCD_9N9vB6L5lMHi9q4LHq-r3jOXyu8ZQ-PzN3JgRuo4aLpLTUqqMvzJNfXAfbHffQWVxR6JjWUJLKykc0lafDw1LglsrDYCrTnTASkkssPwN9wC8jbAtY-5RpRPobbZtyTx-YWD7qXWJDw=s168-no',
+    'photo_url': 'https://lh3.googleusercontent.com/EXNa7IUoKoGfK_ttGsO77ChH4jhZVjuNA9VgZdi-9YC3FLe7okR9r40bFUQ6k74OQqvct6Xtai19vDmirsKnjNuGzVMMzKBIIn7-W6hIjx1kn27EkCUNwfq8WtcRJNtpBI3wL28rATQAp4B6foNIQvNqw3zNp2jMDUTh38brD5WaUOzUUGo0f4dM1-RNVwY46DXkIP0j_S5I6j9gNWFVzNqS1y_HZ53a4ZPTIPB4DYhUh4V-74zWYKg8Sf6QCxx_KDf-lG_MDb3IbU4IZbDnw7g8gUUtgEFQb4vcWSk0Te2LO-8Ffk6rYXAFIgkehRGnSaP91nqga7KXyq0WTnOLxcute_cx18nll9NuBiYPBXcrWIClxQC3oMzaOqdwlNZK84s-YvaiEbbZxQ5seUS6yYXxZGVQrYHZBl9xCla-anNHwIEwk1qZCmprf_3fbJgMJUjxf3JGOFd-Sm9bGm50d1NNRwPyTp2GBI7CzQRkIDxSs-vjkrRV0LILijmVk7pr_maXnDeiR53S4EG7aKaj2S2H6xKE_jF0Uz_AtnKgzvYWtoh5xpIR8fiffyVbsFZrjEF33pTfSKfGfrWXseLgPXg5zur_gPMYaH09d9U=s511-no',
     'video_link': 'https://www.getstoried.com/',
     'video_caption': 'How do you explain what you do?',
     'websites': [{'website': 'https://www.getstoried.com/'}],
     'contact': 'jodi@getstoried.com',
     'publications': [{'publication': 'World Storytelling Day: The Land of Upside Down'}]
-  }
+  },
 
+  {
+    'name': 'GERALD HÜTHER',
+    'bio': 'Gerald Hüther (born February 15, 1951 in Emleben) is a German neurobiologist and author of popular science books and other writings.',
+    'photo_url': 'https://lh3.googleusercontent.com/19KXB0h8S2vtnZl4JVELSpa8vIp5Q7D3BKHuacNmkfxg-3tnJZCRPk2m8M1ocp5ueKDfaMDEYQgOfbhHBST4j4f1kwl-i6ndtMFgqJuEIX8jW3WDJ1u31PWNMJl7M7KXZWJy9zuPJKMoE1S2IomxVQbuv05TuLwAejxe5AYEFsNBb2h40fFBJrjL7cUyxmXrqhX0U0aDbwKUO-CmMzH2_kak7hRbOwCUo4eWLVaJmB9fqbfB1IJkW69Mw2tRBOYWtAwbBHExKIoA3UYXvOsd-4Ixk6sOGmR-y5cH7W5OLmQueXzFqr_QQicGSpeMiYZTycMhDtRmgS2J77GA5XPhWHScIwRjC1rKwWuYYvOAaOaf7H3nynAxteN5a0lm6PFcfcl1vuwsCJKWqvylboxe5Mkl5myJNjsCK69UdG7zQL9_lLEpRV8WlBz0EOj78JO9YJSNXVWyibh184fukQYSbZZvJrtvZz1dSBHF5O-Gr2qau1P2ql6K76vgc9sx9XtyCLS5aUpbfWHPDSMgHHqkvZtuHzryM92Ebj_b2aUP0Gj3f5OXY98dIGvxYzT-yzY8qI2Ot7gMMuM92ALdewP_W5IiaK66facdh2a_DbQ=w349-h334-no',
+    'video_link': 'https://youtu.be/zibjmeGuvbE',
+    'video_caption': ['     Bewusstsein & Verbundenheit         ',
+                      'Wie du dein Potential entwickelte kannst.'],
+    'websites': [{'website': 'https://www.gerald-huether.de/'}, {'website': 'https://de.wikipedia.org/wiki/Gerald_Hüther'}],
+    'contact': 'jodi@getstoried.com',
+    'publications': [{'publication': 'Kommunale Intelligenz. edition Körber-Stiftung'}, {'publication':'Die Macht der inneren Bilder. Wie Visionen das Gehirn, den Menschen und die Welt verändern'}, {'publication': 'Bedienungsanleitung für ein menschliches Gehirn.'}]
+  }
 
 ];
 
