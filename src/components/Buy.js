2a660cab0059561c8c15aa68f2702a040dbf9bfb
Mona Temchin <monatemchin@gmail.com>
2018-09-09 08:41:23 -0400

    Added note on publishing to Buy page, additional styles

--- src/components/Buy.js
+++ src/components/Buy.js
@@ -15,15 +15,37 @@ const Buy = (props) => {
     </div>
     <div className="media-body">
       <div className="row buy">
-        <div className="col-4 offset-5 text-left">
-
+        <div className="col-4 offset-6 text-left buy-col-1">
+          <h4>EBOOK</h4>
+          <br></br>
+          <img src='/images/Amazon.jpg' alt="Amazon" width="80" height="80"/>
+          <br></br>
+          <br></br>
+          <img src='/images/Barnes_and_Noble.jpg' alt="Barnes and Noble" width="80" height="80"/>
+          <br></br>
+          <br></br>
+          <div className="bn-logo">
+             <img src='/images/Barnes_and_Noble_logo.svg' alt='Barnes and Noble logo' width='100' />
+          </div>
+          <div>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <h5 className="book">BOOK</h5>
+            <div className="Amazon-logo">
+            <img src='/images/Amazon_Logo_Transparent.png' alt='Amazon logo' width='170' />
+            </div>
+          </div>
         </div>
-        <div className="col-6 offset-3 text-left">
-
+        <div className="col-5 offset-8 text-left buy-col-2 border border-dark">
+          <h1 className="heading1">NOTE</h1>
         </div>
       </div>
     </div>
-    <div className="bottom-navigation-Buy">
+    <div className="bottom-nav-buy">
       <NavBottomNoToggle page={'Buy'}/>
     </div>
   </div>);
