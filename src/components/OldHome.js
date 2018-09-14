3d305035bc370ef8b1871e82ea01456e0eea4ba2
Mona Temchin <monatemchin@gmail.com>
2018-09-14 01:58:05 -0400

    Refactored home page and home page styles

--- src/components/OldHome.js
+++ src/components/OldHome.js
@@ -14,33 +14,43 @@ const Home = (props) => {
       <div className="media-body ml-3 quotes">
         <div className="row">
           <div className="col-12">
-              <p>The most powerful person in the world
+            <section id="most-powerful-section">
+              <p id="most-powerful">The most powerful person in the world
                 <br></br>is the storyteller. The storyteller sets the
                 <br></br>vision, values, and agenda for an entire
                 <br></br>generation that is to come.
               </p>
-              <p className="famous_people">Steve Jobs</p>
-              <p>If you want to learn about a CULTURE,
+              <p id="steve-jobs">Steve Jobs</p>
+            </section>
+            <section id="group-4">
+              <p id="if-you-want-to-learn">If you want to learn about a CULTURE,
                 <br></br>listen to the stories.If you want to change
                 <br></br>the culture, change the stories.
               </p>
               <br></br>
-              <p className="famous_people">Michael Margolis, Technologist
+              <p id="michael-margolis-te">Michael Margolis, Technologist {/* <p> Michael Margolis, Technologist */}
+                {/* <br></br> */}
               </p>
-              <p>Stories constitute the single most powerful
+            </section>
+            <section id="group-3-section">
+              <p id="stories-constitute-t">Stories constitute the single most powerful
                 <br></br>weapon in a LEADER’s arsenal.
               </p>
               <br></br>
-              <p className="famous_people">Howard Gardner, Psychologist
+              <br></br>
+              <p id="howard-gardner-psyc">Howard Gardner, Psychologist
                 <br></br>
               </p>
+            </section>
+            <section id="its-like-everyone-t-section" className="d-flex flex-column">
               <br></br>
-              <p>It’s like everyone tells a story about
+              <p id="its-like-everyone-t">It’s like everyone tells a story about
                 <br></br>themselves inside their own head…
                 <br></br>We build OURSELVES out of that story.
               </p>
-              <p className="famous_people">Patrick Rothfuss, Writer
+              <p id="patrick-rothfuss-wr">Patrick Rothfuss, Writer
               </p>
+            </section>
           </div>
         </div>
       </div>
