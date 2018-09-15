b0ff56ca1ba718d0f1cecff609f822699cf62fd9
Mona Temchin <monatemchin@gmail.com>
2018-09-15 18:22:44 -0400

    Changed formatting on description page

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import '../styles/Description.css';
 import '../styles/Anthology.css';
-import NavTop from './NavTop.js'
+import {Link} from 'react-router-dom';
+// import NavTop from './NavTop.js'
 import NavBottomNoToggle from './NavBottomNoToggle.js'
 
 const Description = (props) => {
 
   return (<div className="container">
-    <div className="top-nav">
-      <NavTop page={'Description'}/>
+
+    <nav className="navbar navbar-light navbar-expand-sm navtop-desc navtop p-4">
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="navbar-collapse collapse navtop-collapse" id="navbarToggler">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item nav-item-anthology nav-item-top">
+             <Link to="/" className="link-anthology">HOME</Link>
+          </li>
+          <li className="nav-item nav-item-anthology nav-item-top active">
+            <Link to="/description" className="link-anthology">DESCRIPTION</Link>
+          </li>
+          <li  className="nav-item nav-item-anthology nav-item-top">
+            <Link to="/tableofcontents" className="link-anthology">TABLE OF CONTENTS</Link>
+          </li>
+          <li className="nav-item nav-item-anthology nav-item-top">
+            <Link to="/authors" className="link-anthology">AUTHORS</Link>
+          </li>
+          <li className="nav-item nav-item-anthology nav-item-top">
+            <Link to="/buy" className="link-anthology">BUY</Link>
+          </li>
+      </ul>
     </div>
-    <div className="media desc">
+  </nav>
+
+
+    <div className="media ml-5 descr">
       <img className="cover-001" src="/images/FrontCover.jpeg" alt="Anthology Front Cover"/>
     </div>
     <div className="media-body">
@@ -65,7 +90,24 @@ const Description = (props) => {
         </div>
       </div>
       <div className="bottom-nav">
-        <NavBottomNoToggle page={'Description'}/>
+        <nav className="navbar navbar-light navbar-expand-sm bottom-nav-bar mt-2">
+            <ul className="navbar-nav">
+              <li className="nav-item nav-item-anthology nav-item-bottom">
+                <Link to="/about" className="link-anthology">ABOUT</Link>
+              </li>
+              <li className="nav-item nav-item-anthology nav-item-bottom">
+                <Link to="/imprint" className="link-anthology imprint">IMPRINT</Link>
+              </li>
+              <li className="nav-item nav-item-anthology nav-item-bottom">
+                  <p year-2018>© 2018
+                  </p>
+              </li>
+              <li className="nav-item nav-item-anthology nav-item-bottom">
+                <Link to="/contact" className="link-anthology">CONTACT</Link>
+              </li>
+            </ul>
+        </nav>
+
       </div>
     </div>);
 }
