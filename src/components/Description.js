99dc4491fb8e97a5f9b65a6df22e4d8f6fdcdd8d
Mona Temchin <monatemchin@gmail.com>
2018-10-17 20:50:02 -0400

    Removed links to contact and imprint page from Description page

--- src/components/Description.js
+++ src/components/Description.js
@@ -92,14 +92,14 @@ const Description = (props) => {
             <Link to="/about" className="link-anthology">ABOUT</Link>
           </li>
           <li className="nav-item nav-item-anthology nav-item-bottom">
-            <Link to="/imprint" className="link-anthology imprint">IMPRINT</Link>
+            <p className="link-anthology">IMPRINT</p>
           </li>
           <li className="nav-item nav-item-anthology nav-item-bottom">
             <p>© 2018
             </p>
           </li>
           <li className="nav-item nav-item-anthology nav-item-bottom">
-            <Link to="/contact" className="link-anthology">CONTACT</Link>
+            <p>CONTACT</p>
           </li>
         </ul>
       </nav>
